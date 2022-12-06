63ecab03f6e0a4371ea887951fbcde000829939b
engallote <53823323+engallote@users.noreply.github.com>
2022-12-07 06:28:48 +0900

    Add files via upload

---
+++ 26082.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int A = sc.nextInt();
+    	int B = sc.nextInt();
+    	int C = sc.nextInt();
+    	
+    	int a = B / A;
+    	
+    	System.out.println(a * 3 * C);
+	}
+}
