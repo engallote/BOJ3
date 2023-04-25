7100cbca7eaac22f2e4200177c39aeebf2f80782
engallote <53823323+engallote@users.noreply.github.com>
2023-04-26 00:17:01 +0900

    Add files via upload

---
+++ 25400.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0, number = 1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(num == number) {
+				number += 1;
+				++cnt;
+			}
+		}
+		
+		bw.write((N - cnt) + "");
+		bw.flush();
+	}
+}
