a8b6b33f7a6f72f6b760508bb169168fa00b75a5
engallote <53823323+engallote@users.noreply.github.com>
2024-02-20 02:09:36 +0900

    Add files via upload

---
+++ 24484.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static long res, cnt;
+	static ArrayList<Integer>[] aly;
+	static long[] chk, dep;
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		aly = new ArrayList[N + 1];
+		chk = new long[N + 1];
+		dep = new long[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			aly[i] = new ArrayList<>();
+			chk[i] = -1;
+			dep[i] = -1;
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			aly[(int)a].add(b);
+			aly[(int)b].add(a);
+		}
+		
+		for(int i = 1; i <= N; i++) Collections.sort(aly[i]);
+		
+		res = 0;
+		cnt = 2;
+		chk[R] = 1;
+		dep[R] = 0;
+		solve(R, 1);
+		
+		for(int i = 1; i <= N; i++) res += (chk[i] == -1 ? 0 : chk[i]) * dep[i];
+		
+		bw.write(res + "");
+		bw.flush();
+	}
+	private static void solve(int r, int d) {		
+		for(int i = aly[r].size() - 1; i >= 0 ; i--) {
+			int next = aly[r].get(i);
+			
+			if(dep[next] != -1) continue;
+			chk[next] = cnt++;
+			dep[next] = d; 
+			solve(next, d + 1);
+		}
+	}
+}
